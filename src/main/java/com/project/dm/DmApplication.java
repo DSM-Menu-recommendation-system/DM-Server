cdc905525f550ad153fd8c8df1793d4ceb70cbd8
Kangmin <ehfehf0629@gmail.com>
2022-11-17 21:56:48 +0900

    setting: init project

---
+++ src/main/java/com/project/dm/DmApplication.java
@@ -0,0 +1,13 @@
+package com.project.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmApplication.class, args);
+    }
+
+}
