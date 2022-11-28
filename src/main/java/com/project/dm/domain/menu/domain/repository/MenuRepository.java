c3a7156745a1b99e9e9c07cc483dd526745d566e
Kangmin <ehfehf0629@gmail.com>
2022-11-28 09:21:56 +0900

    add: MenuRepository

---
+++ src/main/java/com/project/dm/domain/menu/domain/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.project.dm.domain.menu.domain.repository;
+
+import com.project.dm.domain.menu.domain.Menu;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MenuRepository extends CrudRepository<Menu, Long> {
+}
