25dcc04c9b0f0d918a89b1f42b4d28b1dbc75a79
Kangmin <ehfehf0629@gmail.com>
2022-12-02 16:19:54 +0900

    add: ExceptionFilter

---
+++ src/main/java/com/project/dm/global/error/ExceptionFilter.java
@@ -0,0 +1,48 @@
+package com.project.dm.global.error;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.dm.global.error.exception.BusinessException;
+import com.project.dm.global.error.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (BusinessException e) {
+            sendErrorMessage(response, e.getErrorCode());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            sendErrorMessage(response, ErrorCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private void sendErrorMessage(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+        ErrorResponse errorResponse = ErrorResponse
+                .builder()
+                .status(errorCode.getStatus())
+                .message(errorCode.getMessage())
+                .build();
+
+        String errorResponseJson = objectMapper.writeValueAsString(errorResponse);
+
+        response.setStatus(errorCode.getStatus());
+        response.setContentType("application/json");
+        response.getWriter().write(errorResponseJson);
+    }
+
+}
