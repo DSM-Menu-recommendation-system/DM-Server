1ac46dfb3b254bf75975a9a628040b4639f1ec93
Kangmin <ehfehf0629@gmail.com>
2022-12-02 17:29:05 +0900

    add: RecommendMenu

--- src/main/java/com/project/dm/domain/menu/service/MenuService.java
+++ src/main/java/com/project/dm/domain/menu/service/MenuService.java
@@ -1,4 +1,10 @@
 package com.project.dm.domain.menu.service;
 
+import com.project.dm.domain.menu.presentation.dto.request.RecommendMenuRequest;
+import com.project.dm.domain.menu.presentation.dto.response.RecommendMenuResponse;
+
 public interface MenuService {
+
+    RecommendMenuResponse recommendMenu(RecommendMenuRequest request);
+
 }
