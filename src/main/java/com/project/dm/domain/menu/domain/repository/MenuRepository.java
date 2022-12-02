b5fc52031ee164356a3a782078c8261f1756652f
Kangmin <ehfehf0629@gmail.com>
2022-12-02 19:08:32 +0900

    add: 날씨에 따른 메뉴찾기

--- src/main/java/com/project/dm/domain/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/project/dm/domain/menu/domain/repository/MenuRepository.java
@@ -1,7 +1,13 @@
 package com.project.dm.domain.menu.domain.repository;
 
 import com.project.dm.domain.menu.domain.Menu;
+import com.project.dm.domain.menu.domain.type.WeatherType;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface MenuRepository extends CrudRepository<Menu, Long> {
+
+    Optional<Menu> findByWeatherType(WeatherType weatherType);
+
 }
