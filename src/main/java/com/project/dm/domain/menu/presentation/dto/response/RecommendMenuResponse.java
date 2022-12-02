9d47a1efd7ddf21230cbd34bcdf953ab6d70584e
Kangmin <ehfehf0629@gmail.com>
2022-12-02 19:07:48 +0900

    RecommendMenuResponse

---
+++ src/main/java/com/project/dm/domain/menu/presentation/dto/response/RecommendMenuResponse.java
@@ -0,0 +1,12 @@
+package com.project.dm.domain.menu.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RecommendMenuResponse {
+
+    private final String menu;
+
+}
