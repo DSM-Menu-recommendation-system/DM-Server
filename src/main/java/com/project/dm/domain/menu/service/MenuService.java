cb1bb54eec69e8f5bbea1f2e8a0b610ba33536b4
Kangmin <ehfehf0629@gmail.com>
2022-12-03 09:58:35 +0900

    fix: 이름 변경

--- src/main/java/com/project/dm/domain/menu/service/MenuService.java
+++ src/main/java/com/project/dm/domain/menu/service/MenuService.java
@@ -1,10 +1,11 @@
 package com.project.dm.domain.menu.service;
 
-import com.project.dm.domain.menu.presentation.dto.request.RecommendMenuRequest;
+import com.project.dm.domain.menu.presentation.dto.request.RecommendWeatherMenuRequest;
 import com.project.dm.domain.menu.presentation.dto.response.RecommendMenuResponse;
 
 public interface MenuService {
 
-    RecommendMenuResponse recommendMenu(RecommendMenuRequest request);
+    RecommendMenuResponse recommendMenu(RecommendWeatherMenuRequest request);
+
 
 }
