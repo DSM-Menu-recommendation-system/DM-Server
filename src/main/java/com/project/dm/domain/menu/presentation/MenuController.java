d34cd88bb8ad67fdc8595c2c66ad07bc00954f1f
Kangmin <ehfehf0629@gmail.com>
2022-12-03 09:58:58 +0900

    add: MenuController

---
+++ src/main/java/com/project/dm/domain/menu/presentation/MenuController.java
@@ -0,0 +1,10 @@
+package com.project.dm.domain.menu.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/menus")
+@RestController
+public class MenuController {
+}
