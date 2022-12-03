75dae17c32cc03bef20847f9fa7132bb65f034f1
Kangmin <ehfehf0629@gmail.com>
2022-12-03 10:51:45 +0900

    add: MenuFacade

---
+++ src/main/java/com/project/dm/domain/menu/facade/MenuFacade.java
@@ -0,0 +1,9 @@
+package com.project.dm.domain.menu.facade;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MenuFacade {
+}
