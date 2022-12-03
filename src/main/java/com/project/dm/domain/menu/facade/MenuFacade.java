292db451ee47acb9770df1b49e6046a843461a3c
Kangmin <ehfehf0629@gmail.com>
2022-12-03 10:53:00 +0900

    add: 메소드 추가

--- src/main/java/com/project/dm/domain/menu/facade/MenuFacade.java
+++ src/main/java/com/project/dm/domain/menu/facade/MenuFacade.java
@@ -1,9 +1,27 @@
 package com.project.dm.domain.menu.facade;
 
+import com.project.dm.domain.menu.domain.Menu;
+import com.project.dm.domain.menu.domain.repository.MenuRepository;
+import com.project.dm.domain.menu.domain.type.MoodType;
+import com.project.dm.domain.menu.domain.type.WeatherType;
+import com.project.dm.domain.menu.exception.MenuNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
 public class MenuFacade {
+
+    private final MenuRepository menuRepository;
+
+    public Menu getMenuByWeatherType(WeatherType weatherType) {
+        return menuRepository.findByWeatherType(weatherType)
+                .orElseThrow(() -> MenuNotFoundException.EXCEPTION);
+    }
+
+    public Menu getMenuByMoodType(MoodType moodType) {
+        return menuRepository.findByMoodType(moodType)
+                .orElseThrow(() -> MenuNotFoundException.EXCEPTION);
+    }
+
 }
