80ee1a437da878cd5c0973f3166ef7763627bc13
Kangmin <ehfehf0629@gmail.com>
2022-12-18 21:38:16 +0900

    fix: 메소드 이름 변경

--- src/main/java/com/project/dm/domain/menu/facade/MenuFacade.java
+++ src/main/java/com/project/dm/domain/menu/facade/MenuFacade.java
@@ -2,8 +2,6 @@ package com.project.dm.domain.menu.facade;
 
 import com.project.dm.domain.menu.domain.Menu;
 import com.project.dm.domain.menu.domain.repository.MenuRepository;
-import com.project.dm.domain.menu.domain.type.MoodType;
-import com.project.dm.domain.menu.domain.type.WeatherType;
 import com.project.dm.domain.menu.exception.MenuNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,13 +12,8 @@ public class MenuFacade {
 
     private final MenuRepository menuRepository;
 
-    public Menu getMenuByWeatherType(WeatherType weatherType) {
-        return menuRepository.findByWeatherType(weatherType)
-                .orElseThrow(() -> MenuNotFoundException.EXCEPTION);
-    }
-
-    public Menu getMenuByMoodType(MoodType moodType) {
-        return menuRepository.findByMoodType(moodType)
+    public Menu getMenuById(Integer menuId) {
+        return menuRepository.findById(menuId)
                 .orElseThrow(() -> MenuNotFoundException.EXCEPTION);
     }
 
