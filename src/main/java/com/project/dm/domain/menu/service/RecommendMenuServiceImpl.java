80fdfdf839ff4a16bd65b6afe81c333379269114
Kangmin <ehfehf0629@gmail.com>
2022-12-18 21:38:44 +0900

    fix: random 추가

--- src/main/java/com/project/dm/domain/menu/service/RecommendMenuServiceImpl.java
+++ src/main/java/com/project/dm/domain/menu/service/RecommendMenuServiceImpl.java
@@ -1,12 +1,14 @@
 package com.project.dm.domain.menu.service;
 
 import com.project.dm.domain.menu.domain.Menu;
+import com.project.dm.domain.menu.domain.repository.MenuRepository;
 import com.project.dm.domain.menu.domain.type.MoodType;
 import com.project.dm.domain.menu.domain.type.WeatherType;
 import com.project.dm.domain.menu.facade.MenuFacade;
 import com.project.dm.domain.menu.presentation.dto.request.RecommendMoodMenuRequest;
 import com.project.dm.domain.menu.presentation.dto.request.RecommendWeatherMenuRequest;
 import com.project.dm.domain.menu.presentation.dto.response.RecommendMenuResponse;
+import com.project.dm.global.util.RandomUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,13 +18,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class RecommendMenuServiceImpl implements MenuService {
 
     private final MenuFacade menuFacade;
+    private final MenuRepository menuRepository;
+    private final RandomUtil randomUtil;
 
     @Override
     @Transactional(readOnly = true)
     public RecommendMenuResponse recommendMenu(RecommendWeatherMenuRequest request) {
 
         WeatherType weatherType = request.getWeatherType();
-        Menu menu = menuFacade.getMenuByWeatherType(weatherType);
+
+        Long menuCount = menuRepository.countAllByWeatherType(weatherType);
+        Integer menuId = randomUtil.random(menuCount);
+        System.out.println("menuId = " + menuId);
+        Menu menu = menuFacade.getMenuById(menuId);
 
         return new RecommendMenuResponse(menu.getFood());
     }
@@ -32,7 +40,11 @@ public class RecommendMenuServiceImpl implements MenuService {
     public RecommendMenuResponse recommendMenu(RecommendMoodMenuRequest request) {
 
         MoodType moodType = request.getMoodType();
-        Menu menu = menuFacade.getMenuByMoodType(moodType);
+
+        Long menuCount = menuRepository.countAllByMoodType(moodType);
+        Integer menuId = randomUtil.random(menuCount);
+        System.out.println("menuId = " + menuId);
+        Menu menu = menuFacade.getMenuById(menuId);
 
         return new RecommendMenuResponse(menu.getFood());
     }
