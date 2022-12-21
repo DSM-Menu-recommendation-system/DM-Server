9d6cf8b58f3e1aa788989f9181c9e87edef74822
Kangmin <ehfehf0629@gmail.com>
2022-12-21 16:30:43 +0900

    add: notnull annotation

--- src/main/java/com/project/dm/domain/menu/presentation/dto/request/RecommendMoodMenuRequest.java
+++ src/main/java/com/project/dm/domain/menu/presentation/dto/request/RecommendMoodMenuRequest.java
@@ -4,10 +4,13 @@ import com.project.dm.domain.menu.domain.type.MoodType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class RecommendMoodMenuRequest {
 
+    @NotNull
     private MoodType moodType;
 
 }
