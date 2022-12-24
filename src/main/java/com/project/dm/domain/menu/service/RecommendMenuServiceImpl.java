4a2a2f4ad93404a80ccf01071df5f67e96e28681
Kangmin <ehfehf0629@gmail.com>
2022-12-25 01:47:00 +0900

    fix: MenuService

--- src/main/java/com/project/dm/domain/menu/service/RecommendMenuServiceImpl.java
+++ src/main/java/com/project/dm/domain/menu/service/RecommendMenuServiceImpl.java
@@ -8,18 +8,19 @@ import com.project.dm.domain.menu.facade.MenuFacade;
 import com.project.dm.domain.menu.presentation.dto.request.RecommendMoodMenuRequest;
 import com.project.dm.domain.menu.presentation.dto.request.RecommendWeatherMenuRequest;
 import com.project.dm.domain.menu.presentation.dto.response.RecommendMenuResponse;
-import com.project.dm.global.util.RandomUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
 public class RecommendMenuServiceImpl implements MenuService {
 
+    Random random = new Random();
     private final MenuFacade menuFacade;
     private final MenuRepository menuRepository;
-    private final RandomUtil randomUtil;
 
     @Override
     @Transactional(readOnly = true)
@@ -28,11 +29,11 @@ public class RecommendMenuServiceImpl implements MenuService {
         WeatherType weatherType = request.getWeatherType();
 
         Long menuCount = menuRepository.countAllByWeatherType(weatherType);
-        Integer menuId = randomUtil.random(menuCount);
+        Integer menuId = random.nextInt(7);
         System.out.println("menuId = " + menuId);
-        Menu menu = menuFacade.getMenuById(menuId);
+        Menu menu = menuFacade.getMenuByWeatherNum(menuId);
 
-        return new RecommendMenuResponse(menu.getFood());
+        return new RecommendMenuResponse(menu.getFood(), menu.getMenuImageUrl());
     }
 
     @Override
@@ -42,11 +43,15 @@ public class RecommendMenuServiceImpl implements MenuService {
         MoodType moodType = request.getMoodType();
 
         Long menuCount = menuRepository.countAllByMoodType(moodType);
-        Integer menuId = randomUtil.random(menuCount);
+        Integer menuId = random.nextInt(7);
         System.out.println("menuId = " + menuId);
-        Menu menu = menuFacade.getMenuById(menuId);
+        Menu menu = menuFacade.getMenuByMoodNum(menuId);
 
-        return new RecommendMenuResponse(menu.getFood());
+        return new RecommendMenuResponse(menu.getFood(), menu.getMenuImageUrl());
+    }
+
+    private int random(Long count) {
+        return (int)(random.nextLong(count) + 1);
     }
 
 }
