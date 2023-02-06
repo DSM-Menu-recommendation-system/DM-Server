a5fa5ef7b44267b5aecf301f7aeafd208e48a730
Kangmin <ehfehf0629@gmail.com>
2023-02-06 23:51:59 +0900

    refactor : code

--- src/main/java/com/project/dm/controller/MenuController.java
+++ src/main/java/com/project/dm/controller/MenuController.java
@@ -3,7 +3,7 @@ package com.project.dm.controller;
 import com.project.dm.dto.response.RecommendMenuResponse;
 import com.project.dm.entity.type.MoodType;
 import com.project.dm.entity.type.WeatherType;
-import com.project.dm.service.RecommendMenuServiceImpl;
+import com.project.dm.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MenuController {
 
-    private final RecommendMenuServiceImpl recommendMenuService;
+    private final MenuService menuService;
 
     @GetMapping("/weather")
     public RecommendMenuResponse recommendMenu(@RequestParam(value = "weather_type")WeatherType weatherType) {
-        return recommendMenuService.recommendMenu(weatherType);
+        return menuService.recommendMenu(weatherType);
     }
 
     @GetMapping("/mood")
     public RecommendMenuResponse recommendMenu(@RequestParam(value = "mood_type")MoodType moodType) {
-        return recommendMenuService.recommendMenu(moodType);
+        return menuService.recommendMenu(moodType);
     }
 
 }
